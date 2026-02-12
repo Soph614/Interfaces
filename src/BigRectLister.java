78b3c7e919d426162d5bd46700368ae5f5657c3c
soph <sophia@javamate.net>
2026-02-12 09:08:53 -0500

    Commit 4

--- src/BigRectLister.java
+++ src/BigRectLister.java
@@ -8,16 +8,16 @@ public class BigRectLister {
         // creating an ArrayList for rectangles
         ArrayList<Rectangle> rectangles = new ArrayList<>();
         // creating rectangles
-        Rectangle rectangle1 = new Rectangle(1, 1,1, 1);
-        Rectangle rectangle2 = new Rectangle(2, 2,2, 2);
-        Rectangle rectangle3 = new Rectangle(3, 3,3, 3);
-        Rectangle rectangle4 = new Rectangle(4, 4,4, 4);
-        Rectangle rectangle5 = new Rectangle(5, 5,5, 5);
-        Rectangle rectangle6 = new Rectangle(6, 6,6, 6);
-        Rectangle rectangle7 = new Rectangle(7, 7,7, 7);
-        Rectangle rectangle8 = new Rectangle(8, 8,8, 8);
-        Rectangle rectangle9 = new Rectangle(9, 9,9, 9);
-        Rectangle rectangle10 = new Rectangle(10, 10, 10, 10);
+         Rectangle rectangle1 = new Rectangle(1, 1,1, 1);
+         Rectangle rectangle2 = new Rectangle(2, 2,2, 2);
+         Rectangle rectangle3 = new Rectangle(3, 3,3, 3);
+         Rectangle rectangle4 = new Rectangle(4, 4,4, 4);
+         Rectangle rectangle5 = new Rectangle(5, 5,5, 5);
+         Rectangle rectangle6 = new Rectangle(6, 6,6, 6);
+         Rectangle rectangle7 = new Rectangle(7, 7,1, 2);
+         Rectangle rectangle8 = new Rectangle(8, 8,1, 3);
+         Rectangle rectangle9 = new Rectangle(9, 9,1, 4);
+        Rectangle rectangle10 = new Rectangle(1, 1,2, 3);
         // adding rectangles to "rectangles" arrayList
         rectangles.addFirst((rectangle1));
         rectangles.add((rectangle2));
